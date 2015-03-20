ef2ed39e2abecacf55985841062a70ea07aa3848
David Penso <davidpenso93@gmail.com>
2015-03-20 16:51:42 -0400

    added new york and sanfran

--- eventbrite_project/routes/index.js
+++ eventbrite_project/routes/index.js
@@ -17,26 +17,26 @@ eb_client.event_search( params, function(err, data){
 	//console.log(data.events[2].event.venue);
 	console.log(data.events.length);
 	for (var i=1;i<data.events.length;i++){
-		console.log(data.events[i].event.venue);
+		//console.log(data.events[i].event.start_date);
 		datas.push(data.events[i].event.venue);
 	}
 });
 
 var p = {'region': "CA"};
+var st_date="";
 eb_client.event_search( p, function(err, data){
     //console.log(err);
     //console.log(data);
-    
-	//console.log(data.events[2].event.venue);
-	//console.log(data.events.length);
-	for (var i=1;i<data.events.length;i++){
-		console.log(data.events[i].event.venue);
-		dat.push(data.events[i].event.venue);
-	}
+	
+		console.log(data.events[1].event.venue);
+		st_date=data.events[1].event.start_date;
+		dat.push(data.events[1].event.venue);
+	
 });
+
 router.get('/', function(req, res) {
 	
-  res.render('index', { title: 'Express', ny: datas, ca: dat });
+  res.render('index', { ny: datas, ca: dat, date: st_date });
 });
 
 
